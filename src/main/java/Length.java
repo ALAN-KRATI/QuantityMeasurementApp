4f0aabc6a87bd629e6e7be17aafb15c1d0dc532a
ALAN-KRATI <alankratisaxena@gmail.com>
2026-02-20 15:53:08 +0530

    UC6 - Addition of Two Length Units (Same Category)

--- src/main/java/Length.java
+++ src/main/java/Length.java
@@ -1,10 +1,34 @@
 import java.util.Objects;
 
-public class QuantityLength {
+public class Length {
     private final double value;
     private final LengthUnit unit;
 
-    public QuantityLength(double value, LengthUnit unit){
+    public enum LengthUnit {
+        FEET(1.0),
+        INCH(1.0/12),
+        YARDS(3.0),
+        CENTIMETER(0.03280839895);
+
+        private final double convertToFeet;
+
+        LengthUnit(double convertToFeet){
+            this.convertToFeet = convertToFeet;
+        }
+
+        public double toFeet(double value){
+            return value * convertToFeet;
+        }
+
+        public double fromFeet(double value){
+            return value/convertToFeet;
+        }
+
+        public double getFeetFactor(){ return convertToFeet; }
+
+    }
+
+    public Length(double value, LengthUnit unit){
         this.value = value;
         this.unit = unit;
     }
@@ -13,7 +37,7 @@ public class QuantityLength {
         return unit.toFeet(value);
     }
 
-    public QuantityLength add(QuantityLength q){
+    public Length add(Length q){
         if(q == null){
             throw new IllegalArgumentException("Cannot add null quantity!");
         }
@@ -23,7 +47,7 @@ public class QuantityLength {
         double sum = first + second;
         double ans = this.unit.fromFeet(sum);
 
-        return new QuantityLength(ans, this.unit);
+        return new Length(ans, this.unit);
     }
 
     @Override
@@ -31,7 +55,7 @@ public class QuantityLength {
         if(this == obj) return true;
         if(obj == null || getClass() != obj.getClass()) return false;
 
-        QuantityLength other = (QuantityLength) obj;
+        Length other = (Length) obj;
         return Double.compare(this.toBaseValue(), other.toBaseValue()) == 0;
     }
 
