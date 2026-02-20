30714e9ddfb82a1314f9d5e6415188c4e32b4b8c
ALAN-KRATI <alankratisaxena@gmail.com>
2026-02-20 17:30:57 +0530

    UC10: Generic Quantity Class with Unit Interface for Multi-Category Support

--- src/main/java/QuantityMeasurementApp.java
+++ src/main/java/QuantityMeasurementApp.java
@@ -1,15 +1,21 @@
 public class QuantityMeasurementApp {
+
     public static void main(String[] args) {
-        Weight kg = new Weight(1, WeightUnit.KILOGRAM);
-        Weight g = new Weight(1000, WeightUnit.GRAM);
-        Weight lb = new Weight(2.20462, WeightUnit.POUND);
 
-        System.out.println("1kg == 1000g : " + kg.equals(g));
-        System.out.println("1kg == 2.20462lb : " + kg.equals(lb));
+        Quantity<LengthUnit> l1 = new Quantity<>(1, LengthUnit.FEET);
+        Quantity<LengthUnit> l2 = new Quantity<>(12, LengthUnit.INCH);
 
-        System.out.println("1kg + 500g = " + kg.add(new Weight(500, WeightUnit.GRAM)));
-        System.out.println("1kg + 1lb (in pounds) = " +
-        Weight.add(kg, new Weight(1, WeightUnit.POUND), WeightUnit.POUND));
+        System.out.println("Length equal? " + l1.equals(l2));
 
+        Quantity<WeightUnit> w1 = new Quantity<>(1, WeightUnit.KILOGRAM);
+        Quantity<WeightUnit> w2 = new Quantity<>(1000, WeightUnit.GRAM);
+
+        System.out.println("Weight equal? " + w1.equals(w2));
+
+        System.out.println("Convert 1 foot to inches: " + l1.convertTo(LengthUnit.INCH));
+
+        System.out.println("Add lengths: " + l1.add(l2));
+
+        System.out.println("Add weights in grams: " + w1.add(w2, WeightUnit.GRAM));
     }
 }
--- src/main/java/WeightUnit.java
+++ src/main/java/WeightUnit.java
@@ -1,4 +1,4 @@
-public enum WeightUnit {
+public enum WeightUnit implements IMeasurable{
     KILOGRAM(1.0),
     GRAM(0.001),
     POUND(0.453592);
@@ -9,13 +9,18 @@ public enum WeightUnit {
         this.toKGFactor = toKGFactor;
     }
 
-    public double convertToKg(double value){
+    public double convertToBase(double value){
         return value * toKGFactor;
     }
 
-    public double convertFromKg(double value){
+    public double convertFromBase(double value){
         return value/toKGFactor;
     }
 
-    public double getToKgFactor() { return toKGFactor; }
+    public double getConversionFactor() { return toKGFactor; }
+
+    @Override
+    public String getUnitName() {
+        return this.name();
+    }
 }
