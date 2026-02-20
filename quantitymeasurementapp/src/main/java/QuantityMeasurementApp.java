9feabb6443c0d4c1a1cf48b50236b0d84b1ab20f
ALAN-KRATI <alankratisaxena@gmail.com>
2026-02-20 11:52:33 +0530

    UC9- Added and implemented the weight enum and service class with tests

--- quantitymeasurementapp/src/main/java/QuantityMeasurementApp.java
+++ quantitymeasurementapp/src/main/java/QuantityMeasurementApp.java
@@ -1,14 +1,15 @@
 public class QuantityMeasurementApp {
     public static void main(String[] args) {
-        QuantityLength feet = new QuantityLength(1.0, LengthUnit.FEET);
-        QuantityLength inch = new QuantityLength(12.0, LengthUnit.INCH);
+        QuantityWeight kg = new QuantityWeight(1, WeightUnit.KILOGRAM);
+        QuantityWeight g = new QuantityWeight(1000, WeightUnit.GRAM);
+        QuantityWeight lb = new QuantityWeight(2.20462, WeightUnit.POUND);
 
-        System.out.println("UC6 result (default LengthUnit): " + feet.add(inch));
+        System.out.println("1kg == 1000g : " + kg.equals(g));
+        System.out.println("1kg == 2.20462lb : " + kg.equals(lb));
 
-        System.out.println("Target FEET: " + QuantityLength.add(feet, inch, LengthUnit.FEET));
-        System.out.println("Target INCH: " + QuantityLength.add(feet, inch, LengthUnit.INCH));
-        System.out.println("Target YARDS: " + QuantityLength.add(feet, inch, LengthUnit.YARDS));
-        System.out.println("Target CM: " + QuantityLength.add(feet, inch, LengthUnit.CENTIMETER));
+        System.out.println("1kg + 500g = " + kg.add(new QuantityWeight(500, WeightUnit.GRAM)));
+        System.out.println("1kg + 1lb (in pounds) = " +
+        QuantityWeight.add(kg, new QuantityWeight(1, WeightUnit.POUND), WeightUnit.POUND));
 
     }
 }
